d8fc74db92194189a3d6b4ab4638ec5c1b9bbfad
Jorge Raúl Díaz Samaniego <jorgediazsamaniego@gmail.com>
2020-09-27 21:03:19 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+const selectMediaStream = async () => {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+button.addEventListener("click", async () => {
+  button.disabled = true;
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
+
+selectMediaStream();
